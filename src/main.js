22c60a621bfc97e27de67367efc503ea94373cd0
YoungChen96 <chenyaoming96@gmail.com>
2018-05-10 23:08:19 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+// import vuex from './store'
+
+Vue.config.productionTip = false
+
+// Vue.use(VueRouter)
+Vue.use(iView)
+
+// const RouterConfig = {
+//   routes: Routers
+// }
+// const router = new VueRouter(RouterConfig)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  // store: vuex,
+  components: { App },
+  template: '<App/>'
+})
